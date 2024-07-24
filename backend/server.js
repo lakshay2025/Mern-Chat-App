88251ab77b9d6fafd083757fd8a62ede497cfaab
lakshay2025 <madhadchiku@gmail.com>
2024-07-24 23:58:42 +0530

    updated once more 3

--- backend/server.js
+++ backend/server.js
@@ -22,18 +22,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-if(process.env.NODE_ENV === "production"){
+
 
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 	
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
-}else{
-	app.get("/", (req, res) => {
-		res.send("API is Running");
-	});
-}
+
 
 server.listen(PORT, () => {
 	connectToMongoDB();
